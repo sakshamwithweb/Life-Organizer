7ac4f0ff2c6de668c060d947582eb57dd453172a
sakshamwithweb <websaksham39@gmail.com>
2024-12-01 12:35:03 +0530

    try1

--- app/api/commitment/route.js
+++ app/api/commitment/route.js
@@ -24,7 +24,7 @@ export async function POST(req) {
                     content: "You are a helpful assistant who extracts the commitment from the conversations done today. You write plain text, No formatting.",
                 },
                 {
-                    role: "user", content: `Today is ${new Date().toDateString()}.Extract all commitments from the following text. For each identified commitment, create an array of objects with the following structure: [{ dueOn: "deadline", title: "string" }, { dueOn: "deadline", title: "string" }]. If no commitments are found in the text, return the message Null. Do not use any formatting like Markdown or special characters, just plain text.\nConversations:${JSON.stringify(data)}`
+                    role: "user", content: `Today is ${new Date().toDateString()}.Extract all commitments from the following text. For each identified commitment, create an array of objects with the following structure: [{ dueOn: "deadline", title: "string" }, { dueOn: "deadline", title: "string" }]. If no commitments are found in the text, return the message Null. Do not use any formatting like Markdown or special characters, just plain text.EIther give a plain array or Null , nothing else ,not even a space.\nConversations:${JSON.stringify(data)}`
                 },
             ],
             model: "gpt-4",
