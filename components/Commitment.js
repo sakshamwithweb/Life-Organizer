e06d160b412ac452eee50d294dd476f584086181
sakshamwithweb <websaksham39@gmail.com>
2024-12-01 12:58:20 +0530

    troed t solve an err

--- components/Commitment.js
+++ components/Commitment.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Commitment = ({ uid }) => {
     const [commitments, setCommitments] = useState([])
